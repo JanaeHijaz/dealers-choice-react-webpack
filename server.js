d55b18230773ab370b132b0369b6d874dca694a2
Janae Edwards <edwards.janae@gmail.com>
2022-03-07 15:44:39 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+// ------ data layer (create models) 
+
+// sequelize required 
+// new Sequelize
+
+
+// ------ express (init function/initialize and port set up)
+
+// express required
+// app = express()
+
+// ------- GET/POST routes and middleware here. 
---
+++ src/index.js
@@ -0,0 +1,11 @@
+// import React, axios, and other stateless components here
+
+// Main Component w/ primary render function
+
+//separate other components as needed 
+
+ReactDOM.render(
+    <Main />,
+    document.getElementById('app')
+  )
+
