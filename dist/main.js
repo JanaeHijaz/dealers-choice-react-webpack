dee8e66d1a5f504e0ae210db2c42c4699257928c
Janae Edwards <edwards.janae@gmail.com>
2022-03-10 04:08:25 -0500

    component bugs!

--- dist/main.js
+++ dist/main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("// import React, axios, and other stateless components here\n// Main Component w/ primary render function\n//separate other components as needed \nReactDOM.render( /*#__PURE__*/React.createElement(Main, null), document.getElementById('#root'));\n\n//# sourceURL=webpack://dealers-choice-react-webpack/./src/index.js?");
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nSyntaxError: /Users/janaehedwards/Fullstack-Academy/Fullstack-Junior-Phase/dealers-choice-react-webpack2/src/index.js: Unexpected token (13:2)\\n\\n\\u001b[0m \\u001b[90m 11 |\\u001b[39m   \\u001b[36mreturn\\u001b[39m (\\u001b[0m\\n\\u001b[0m \\u001b[90m 12 |\\u001b[39m     \\u001b[0m\\n\\u001b[0m\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 13 |\\u001b[39m   )\\u001b[0m\\n\\u001b[0m \\u001b[90m    |\\u001b[39m   \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 14 |\\u001b[39m }\\u001b[0m\\n\\u001b[0m \\u001b[90m 15 |\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 16 |\\u001b[39m \\u001b[90m// Main Component w/ primary render function\\u001b[39m\\u001b[0m\\n    at Object._raise (/Users/janaehedwards/Fullstack-Academy/Fullstack-Junior-Phase/dealers-choice-react-webpack2/node_modules/@babel/parser/lib/index.js:476:17)\\n    at Object.raiseWithData (/Users/janaehedwards/Fullstack-Academy/Fullstack-Junior-Phase/dealers-choice-react-webpack2/node_modules/@babel/parser/lib/index.js:469:17)\\n    at Object.raise (/Users/janaehedwards/Fullstack-Academy/Fullstack-Junior-Phase/dealers-choice-react-webpack2/node_modules/@babel/parser/lib/index.js:430:17)\\n    at Object.unexpected (/Users/janaehedwards/Fullstack-Academy/Fullstack-Junior-Phase/dealers-choice-react-webpack2/node_modules/@babel/parser/lib/index.js:3789:16)\\n    at Object.parseParenAndDistinguishExpression (/Users/janaehedwards/Fullstack-Academy/Fullstack-Junior-Phase/dealers-choice-react-webpack2/node_modules/@babel/parser/lib/index.js:12922:12)\\n    at Object.parseExprAtom (/Users/janaehedwards/Fullstack-Academy/Fullstack-Junior-Phase/dealers-choice-react-webpack2/node_modules/@babel/parser/lib/index.js:12476:23)\\n    at Object.parseExprAtom (/Users/janaehedwards/Fullstack-Academy/Fullstack-Junior-Phase/dealers-choice-react-webpack2/node_modules/@babel/parser/lib/index.js:7812:20)\\n    at Object.parseExprSubscripts (/Users/janaehedwards/Fullstack-Academy/Fullstack-Junior-Phase/dealers-choice-react-webpack2/node_modules/@babel/parser/lib/index.js:12149:23)\\n    at Object.parseUpdate (/Users/janaehedwards/Fullstack-Academy/Fullstack-Junior-Phase/dealers-choice-react-webpack2/node_modules/@babel/parser/lib/index.js:12129:21)\\n    at Object.parseMaybeUnary (/Users/janaehedwards/Fullstack-Academy/Fullstack-Junior-Phase/dealers-choice-react-webpack2/node_modules/@babel/parser/lib/index.js:12104:23)\");\n\n//# sourceURL=webpack://dealers-choice-react-webpack/./src/index.js?");
 
 /***/ })
 
@@ -24,7 +24,7 @@ eval("// import React, axios, and other stateless components here\n// Main Compo
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
 /******/ 	var __webpack_exports__ = {};
 /******/ 	__webpack_modules__["./src/index.js"]();
 /******/ 	
--- src/index.js
+++ src/index.js
@@ -1,6 +1,59 @@
 // import React, axios, and other stateless components here
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+//functional component (child)
+
+const FlightList = (props) => {
+  const flight = props.flight;
+  const destroy = props.destroy; // check this
+  return (
+ // finish this
+  )
+}
 
 // Main Component w/ primary render function
+
+class Main extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      flights: []
+    }
+  }
+  async componentDidMount() {
+    try{
+      const response = await axios.get('/api/flights');
+      this.setState({flights: flights});
+    }
+    catch(error){
+      next(error)
+    }
+  }
+
+  async create () {} // finish
+  async destroy() {} // finish
+
+    render(){
+      return (
+        <div id='flight'>
+          <h1> Fullstack International Airport </h1>
+          <ul>
+            {this.state.flights.map(flight => {
+              return ( 
+                <FlightList />
+              )
+            })}
+          </ul>
+
+        </div>
+      )
+    }
+}
+
+
+
 
 //separate other components as needed 
 
